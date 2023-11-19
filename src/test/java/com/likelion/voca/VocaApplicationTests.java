6cb170e7b357409b1a065a2b3b532d0f6c026811
CANADAGAGOPA <kswms403@naver.com>
2023-11-19 21:25:36 +0900

    프로젝트 생성

---
+++ src/test/java/com/likelion/voca/VocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.voca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
