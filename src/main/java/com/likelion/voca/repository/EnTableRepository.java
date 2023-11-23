3e1af15b0f4d3fe50aff6839c4c19a6f656f947d
CANADAGAGOPA <kswms403@naver.com>
2023-11-23 16:47:31 +0900

    레포지토리 수정

--- src/main/java/com/likelion/voca/repository/EnTableRepository.java
+++ src/main/java/com/likelion/voca/repository/EnTableRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnTableRepository extends JpaRepository<EnTable, Integer> {
 
-    Page<EnTable> findByMeaningContaining(String searchKeyword, Pageable pageable);
-    Page<CnTable> findByWordContaining(String searchKeyword, Pageable pageable);
+    List<EnTable> findByMeaningContaining(String searchKeyword);
+    List<EnTable> findByWordContaining(String searchKeyword);
 }
