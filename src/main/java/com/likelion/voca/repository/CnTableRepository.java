123159621284d59f1322499e36a226858d9e37f1
CANADAGAGOPA <kswms403@naver.com>
2023-11-23 16:42:09 +0900

    레포지토리 수정

--- src/main/java/com/likelion/voca/repository/CnTableRepository.java
+++ src/main/java/com/likelion/voca/repository/CnTableRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CnTableRepository extends JpaRepository<CnTable, Integer> {
     // Jpa 레포지토리를 상속, 제네릭<>으로 엔티티와 pk로 지정한 값의 타입을 지정해준다.
 
-    Page<CnTable> findByMeaningContaining(String searchKeyword, Pageable pageable);
-    Page<CnTable> findByWordContaining(String searchKeyword, Pageable pageable);
+    List<CnTable> findByMeaningContaining(String searchKeyword);
+    List<CnTable> findByWordContaining(String searchKeyword);
 }
--- src/main/java/com/likelion/voca/repository/JpTableRepository.java
+++ src/main/java/com/likelion/voca/repository/JpTableRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JpTableRepository extends JpaRepository<JpTable, Integer> {
 
-    Page<JpTable> findByMeaningContaining(String searchKeyword, Pageable pageable);
-    Page<CnTable> findByWordContaining(String searchKeyword, Pageable pageable);
+    List<JpTable> findByMeaningContaining(String searchKeyword);
+    List<JpTable> findByWordContaining(String searchKeyword);
 }
