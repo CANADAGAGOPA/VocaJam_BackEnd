6fc148fd27ef0d1f21b9fde2425181051524590c
CANADAGAGOPA <kswms403@naver.com>
2023-11-26 19:06:35 +0900

    단어 저장 기능 추가

--- src/main/java/com/likelion/voca/controller/WordsController.java
+++ src/main/java/com/likelion/voca/controller/WordsController.java
@@ -6,10 +6,9 @@ import com.likelion.voca.entity.JpTable;
 import com.likelion.voca.service.VocaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -59,5 +58,29 @@ public class WordsController {
 
         // 데이터가 존재할 경우 OK 상태코드와 함께 데이터 반환
         return ResponseEntity.ok(jpTableFields);
+    }
+
+    // "/words/cn" 엔드포인트로 POST 요청이 오면 중국어 단어를 저장
+    @PostMapping("/cn")
+    public ResponseEntity<Void> updateCnTable(@RequestBody CnTable cnTable) throws IOException {
+        // 받아온 중국어 단어 정보를 서비스를 통해 저장
+        vocaService.cnWrite(cnTable);
+        return ResponseEntity.ok().build();
+    }
+
+    // "/words/en" 엔드포인트로 POST 요청이 오면 영어 단어를 저장
+    @PostMapping("/en")
+    public ResponseEntity<Void> updateEnTable(@RequestBody EnTable enTable) throws IOException {
+        // 받아온 영어 단어 정보를 서비스를 통해 저장
+        vocaService.enWrite(enTable);
+        return ResponseEntity.ok().build();
+    }
+
+    // "/words/jp" 엔드포인트로 POST 요청이 오면 일본어 단어를 저장
+    @PostMapping("/jp")
+    public ResponseEntity<Void> updateJpTable(@RequestBody JpTable jpTable) throws IOException {
+        // 받아온 일본어 단어 정보를 서비스를 통해 저장
+        vocaService.jpWrite(jpTable);
+        return ResponseEntity.ok().build();
     }
 }
