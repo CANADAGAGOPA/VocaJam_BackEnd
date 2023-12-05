6a0a48150aefbfce19f56edec343b6d6f4dd7bd7
CANADAGAGOPA <kswms403@naver.com>
2023-12-05 11:55:16 +0900

    Congig 패키지 및 클래스 추가

---
+++ src/main/java/com/likelion/voca/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.likelion.voca.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/words/**")
+                        .allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
---
+++ src/main/java/com/likelion/voca/config/CustomCorsConfig.java
@@ -0,0 +1,25 @@
+package com.likelion.voca.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomCorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer customCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // 실제 API 엔드포인트에 맞게 설정
+                        .allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
