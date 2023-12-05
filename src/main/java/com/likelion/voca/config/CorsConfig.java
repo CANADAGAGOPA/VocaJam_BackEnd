ad2094b4f2841ddde7db133ce82732ff33f02b02
CANADAGAGOPA <kswms403@naver.com>
2023-12-05 17:37:34 +0900

    Congig 패키지 예하 클래스 주석 추가

--- src/main/java/com/likelion/voca/config/CorsConfig.java
+++ src/main/java/com/likelion/voca/config/CorsConfig.java
@@ -6,18 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+@Configuration // 해당 클래스가 Spring 의 설정 클래스임을 나타낸다.
+@EnableWebMvc // Spring MVC 를 사용
+public class CorsConfig implements WebMvcConfigurer { // CORS 를 구성하기 위한 설정 클래스
 
-    @Bean
+    @Bean // corsConfigurer 메서드를 빈으로 등록
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/words/**")
                         .allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드를 설정
                         .allowedHeaders("*")
                         .allowCredentials(true)
                         .maxAge(3600);
--- src/main/java/com/likelion/voca/config/CustomCorsConfig.java
+++ src/main/java/com/likelion/voca/config/CustomCorsConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CustomCorsConfig implements WebMvcConfigurer {
+public class CustomCorsConfig implements WebMvcConfigurer { // CORS 설정을 커스터마이징하기 위한 구성 클래스
 
     @Bean
     public WebMvcConfigurer customCorsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) { // CORS 관련 설정을 구성
                 registry.addMapping("/api/**") // 실제 API 엔드포인트에 맞게 설정
                         .allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
